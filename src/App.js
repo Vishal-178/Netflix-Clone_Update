d8857c16d6b0d494ae957d7ece3d2c4324949237
vishal singh <vishalkumarmps@gmail.com>
2022-06-05 16:18:11 +0530

    completed faq component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { JumbotronContainer } from "./containers/jumbotrons";
 import { FooterContainer } from "./containers/footer";
+import { FaqsContainer } from "./containers/faq";
 function App() {
   return (
     <>
       <JumbotronContainer />
+      <FaqsContainer />
       <FooterContainer />
     </>
   );
