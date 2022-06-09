081ffad2696ea583d35b32b6a3de3cc406393685
vishal singh <vishalkumarmps@gmail.com>
2022-06-09 15:26:05 +0530

    setting the user, if user is signin, user stay on browser page, if it try to move to signin, signup, or home page it redirect to browser page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import { Home, Browser, SignIn, SignUp } from "./pages";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { useAuthListener } from "./hooks";
+
 function App() {
+  const { user } = useAuthListener();
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/browser" element={<Browser />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
+          <Route
+            path="/browser"
+            element={user ? <Browser /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/"
+            element={user ? <Navigate to="/browser" replace /> : <Home />}
+          />
+          <Route
+            path="/signin"
+            element={user ? <Navigate to="/browser" /> : <SignIn />}
+          />
+          <Route
+            path="/signup"
+            element={user ? <Navigate to="/browser" /> : <SignUp />}
+          />
+          <Route
+            path="*"
+            element={user ? <Navigate to="/browser" /> : <Navigate to="/" />}
+          />
         </Routes>
       </Router>
     </>
