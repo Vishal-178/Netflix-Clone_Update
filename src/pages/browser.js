52ac10f37495b435a7d04455e7d1ee1c57d8b6a5
vishal singh <vishalkumarmps@gmail.com>
2022-06-10 23:57:48 +0530

    created user profile display, after login or refresh, created a loding while user found

--- src/pages/browser.js
+++ src/pages/browser.js
@@ -1,35 +1,15 @@
 import React from "react";
 import useContent from "../hooks/use-content";
+import { selectionFilter } from "../utils";
+import { BrowseContainer } from "../containers/browse";
 export default function Browser() {
   const { series } = useContent("series");
-  console.log("series", series);
   const { films } = useContent("films");
-  console.log("films", films);
-  return (
-    <div>
-      <h1>Browser</h1>
-      <h2>Series</h2>
-      {!series ? (
-        <h1> series not found</h1>
-      ) : (
-        series.map((serie) => (
-          <div key={serie.docId}>
-            <h3>{serie.title}</h3>
-            <p>{serie.description}</p>
-          </div>
-        ))
-      )}
-      <h2>Films</h2>
-      {!films ? (
-        <h1> films not found</h1>
-      ) : (
-        films.map((film) => (
-          <div key={film.docId}>
-            <h3>{film.title}</h3>
-            <p>{film.description}</p>
-          </div>
-        ))
-      )}
-    </div>
-  );
+  let slides = [];
+  if (series && films) {
+    slides = selectionFilter({ series, films });
+    console.log("slides", slides);
+  }
+
+  return <BrowseContainer slides={slides} />;
 }
